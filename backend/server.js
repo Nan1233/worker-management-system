4ac95c9f810814fac66e97139867654913a45b68
Admin <admin@example.com>
2026-07-12 22:57:46 +0700

    fix cors production render

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,24 @@ const productionRoutes = require("./routes/productionRoutes");
 
 const app = express();
 
-app.use(cors());
+
+// CORS CONFIG
+const corsOptions = {
+    origin: [
+        "http://localhost:5173",
+        "https://worker-management-system-3-dzox.onrender.com"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+};
+
+app.use(cors(corsOptions));
+
+// xử lý preflight request
+app.options("*", cors(corsOptions));
+
+
 app.use(express.json());
 
 
