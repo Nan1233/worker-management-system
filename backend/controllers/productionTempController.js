79868e73ec950dec06985689ab2a00b19a6a1c7b
an <nan978971@gmail.com>
2026-07-14 09:59:10 +0700

    fix production temp model import

--- backend/controllers/productionTempController.js
+++ backend/controllers/productionTempController.js
@@ -1,5 +1,4 @@
-const ProductionTemp = require("../models/productionTempModule");
-
+const ProductionTemp = require("../models/productionTempModel");
 
 
 // =====================================================
