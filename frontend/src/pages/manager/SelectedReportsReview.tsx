3e9fe21d7f27bbbfe0c32fa5699b9b4506f6a555
Nan1233 <nan978971@gmail.com>
2026-08-10 17:22:58 +0700

    Fix manager review Axios TypeScript build error

--- frontend/src/pages/manager/SelectedReportsReview.tsx
+++ frontend/src/pages/manager/SelectedReportsReview.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useMemo, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import axios from "axios";
 import {
     approveSelectedTempReports,
     getReportById,
