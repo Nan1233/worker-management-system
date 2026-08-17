c40da8704ac9832185602d9cc11e19b3aa4bea53
Nan1233 <nan978971@gmail.com>
2026-08-17 17:15:03 +0700

    style(worker-form): load redesigned worker form styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,6 +14,7 @@ import "./styles/worker-final-ui.css";
 import "./styles/light-contrast-hardening.css";
 import "./styles/interaction-hardening.css";
 import "./styles/management-layout-failsafe.css";
+import "./styles/worker-form-redesign.css";
 import { ToastProvider } from "./components/feedback/ToastProvider";
 import AuthBootstrap from "./components/AuthBootstrap";
 import AppErrorBoundary from "./components/system/AppErrorBoundary";
