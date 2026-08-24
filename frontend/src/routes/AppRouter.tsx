de6d7fceb7a7c0b77e63d9b88924bebd1bda1e1e
Nan1233 <nan978971@gmail.com>
2026-08-24 08:32:20 +0700

    refactor(ui): remove data standard route and split master pages

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -10,7 +10,6 @@ import RouteLoading from "../components/system/RouteLoading";
 const Login = lazy(() => import("../pages/Login"));
 const AdminDashboard = lazy(() => import("../pages/admin/Dashboard"));
 const MasterData = lazy(() => import("../pages/admin/MasterData"));
-const DataStandard = lazy(() => import("../pages/manager/DataStandard"));
 const FormulaSettings = lazy(() => import("../pages/admin/FormulaSettings"));
 const Governance = lazy(() => import("../pages/admin/Governance"));
 const Permissions = lazy(() => import("../pages/admin/Permissions"));
@@ -48,29 +47,27 @@ export default function AppRouter(){
    <Route path="approved" element={<P code="REPORT_APPROVED_VIEW"><ApprovedReports/></P>}/>
    <Route path="export" element={<P code="REPORT_EXPORT"><ReportDownload/></P>}/>
    <Route path="workers" element={<P code="USER_VIEW"><Workers/></P>}/>
-   <Route path="data-standard" element={<P code="MASTER_VIEW"><DataStandard/></P>}/>
-   <Route path="master" element={<Navigate to="processes" replace/>}/>
+   <Route path="master" element={<Navigate to="machines" replace/>}/>
    <Route path="master/:resource" element={<P code="MASTER_VIEW"><MasterData/></P>}/>
    <Route path="formulas" element={<P code="FORMULA_VIEW"><FormulaSettings/></P>}/>
    <Route path="governance" element={<P code="GOVERNANCE_VIEW"><Governance/></P>}/>
    <Route path="statistics" element={<P code="STATISTICS_VIEW"><Statistics/></P>}/>
    <Route path="permissions" element={<P code="PERMISSION_MANAGE"><Permissions/></P>}/>
-   <Route path="system" element={<P code="NOTIFICATION_VIEW"><SystemCenter/></P>}/>
+   <Route path="system" element={<P code="AUDIT_VIEW"><SystemCenter/></P>}/>
   </Route>
 
   <Route path="/manager" element={<PrivateRoute allowedRoles={["manager"]}><ManagementLayout role="manager"/></PrivateRoute>}>
    <Route index element={<P code="DASHBOARD_VIEW"><ManagerDashboard/></P>}/>
    <Route path="reports" element={<P code="REPORT_PENDING_VIEW"><ManagerReports/></P>}/>
    <Route path="approved" element={<P code="REPORT_APPROVED_VIEW"><ManagerApprovedReports/></P>}/>
-   <Route path="export" element={<Navigate to="/manager/data-standard" replace/>}/>
+   <Route path="export" element={<Navigate to="/manager/master/machines" replace/>}/>
    <Route path="workers" element={<P code="USER_VIEW"><Workers/></P>}/>
-   <Route path="data-standard" element={<P code="MASTER_VIEW"><DataStandard/></P>}/>
-   <Route path="master" element={<Navigate to="processes" replace/>}/>
+   <Route path="master" element={<Navigate to="machines" replace/>}/>
    <Route path="master/:resource" element={<P code="MASTER_VIEW"><MasterData/></P>}/>
    <Route path="formulas" element={<P code="FORMULA_VIEW"><FormulaSettings/></P>}/>
    <Route path="governance" element={<P code="GOVERNANCE_VIEW"><Governance/></P>}/>
    <Route path="statistics" element={<P code="STATISTICS_VIEW"><Statistics/></P>}/>
-   <Route path="system" element={<P code="NOTIFICATION_VIEW"><SystemCenter/></P>}/>
+   <Route path="system" element={<P code="AUDIT_VIEW"><SystemCenter/></P>}/>
   </Route>
 
   <Route path="/lead" element={<PrivateRoute allowedRoles={["lead"]}><ManagementLayout role="lead"/></PrivateRoute>}>
