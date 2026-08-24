9e6135a4c928b182e9ce9c0e925c1689cc1bc7de
Nan1233 <nan978971@gmail.com>
2026-08-24 15:06:13 +0700

    invalidate cache after pending filter fix

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,4 +1,4 @@
-const BUILD_VERSION = "1.9.23-period-filter-20260824";
+const BUILD_VERSION = "1.9.24-period-filter-20260824";
 const APP_CACHE = `ktc-${BUILD_VERSION}-app`;
 const STATIC_CACHE = `ktc-${BUILD_VERSION}-static`;
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./offline.html"];
