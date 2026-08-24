495de381d1e1b54c1d2a69e0da18971ed4d90daa
Nan1233 <nan978971@gmail.com>
2026-08-24 21:43:23 +0700

    style(frontend): load final UI polish v3

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -16,6 +16,7 @@ import OfflineReportSync from "./components/system/OfflineReportSync";
 import ExcelWorkflowTools from "./components/system/ExcelWorkflowTools";
 import "./ui-polish.css";
 import "./ui-polish-v2.css";
+import "./ui-polish-v3.css";
 import { FRONTEND_VERSION, FRONTEND_COMMIT_SHA } from "./config/version";
 
 const initialTheme = localStorage.getItem("ktcTheme");
