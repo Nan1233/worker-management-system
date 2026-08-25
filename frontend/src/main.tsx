1e07eed03115e56a1061745c60460967191438c8
Nan1233 <nan978971@gmail.com>
2026-08-25 15:42:41 +0700

    fix: normalize leading zeros in report quantities

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -18,6 +18,7 @@ import ExcelWorkflowTools from "./components/system/ExcelWorkflowTools";
 import "./ui-polish.css";
 import "./admin-worker-password-override.css";
 import "./config/workerAccountPolicy";
+import "./utils/reportNumberInputFix";
 import { FRONTEND_VERSION, FRONTEND_COMMIT_SHA } from "./config/version";
 
 // KTC currently uses a single enterprise-light visual system.
