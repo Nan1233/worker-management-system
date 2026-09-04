69921435c47b2f99139928e83d2b4053c12908c4
Nan1233 <nan978971@gmail.com>
2026-09-04 11:44:00 +0700

    fix(manager): apply compact pending report detail styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -15,6 +15,7 @@ import "./pages/manager/ApprovedReportsTabExact.css";
 import "./pages/manager/ReportsPeriodFix.css";
 import "./pages/manager/ApprovedReportsSearchFix.css";
 import "./pages/manager/WorkersSearchFix.css";
+import "./pages/manager/ReportsDetailCompact.css";
 import { ToastProvider } from "./components/feedback/ToastProvider";
 import AuthBootstrap from "./components/AuthBootstrap";
 import AppErrorBoundary from "./components/system/AppErrorBoundary";
