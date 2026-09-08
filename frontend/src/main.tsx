f775b7bde8272bf33e26dd76068e6ba30842b6c8
Nan1233 <nan978971@gmail.com>
2026-09-08 14:23:04 +0700

    style(manager): apply unified management UI system

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -20,6 +20,7 @@ import "./pages/manager/WorkersSearchFix.css";
 import "./pages/manager/ReportsDetailCompact.css";
 import "./pages/manager/PendingReportActiveRow.css";
 import "./pages/manager/PendingReportDetailFocus";
+import "./styles/ManagementUnified.css";
 import { ToastProvider } from "./components/feedback/ToastProvider";
 import AuthBootstrap from "./components/AuthBootstrap";
 import AppErrorBoundary from "./components/system/AppErrorBoundary";
